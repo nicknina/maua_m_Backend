c936258ba53d1f5ba30ffc24f32c0864458ef69e
nicknina <nicolas.alencar@gmail.com>
2025-05-12 16:32:22 -0300

    autenticação

---
+++ demo/src/main/java/maua/moove/backend/demo/Autenticação e autorização/AuthController.java
@@ -0,0 +1,37 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/login")
+    public String createAuthenticationToken(@RequestBody AuthRequest authRequest) throws Exception {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
+        return jwtUtil.generateToken(userDetails.getUsername());
+    }
+}
+
+class AuthRequest {
+    private String username;
+    private String password;
+
+    
+}
